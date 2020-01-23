58237adeae79fa1121ed4ae862db1e46d9cc4821
Aldi Hardiansyah <53806974+hard77@users.noreply.github.com>
2020-01-23 15:10:07 +0700

    Update url.js

--- src/components/url.js
+++ src/components/url.js
@@ -7,11 +7,11 @@
 //  export const serverUrl="https://virtserver.swaggerhub.com/ayannahindonesia/Asira_Lender/1.0.0/"
 //  export const serverUrlBorrower="https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/"
 
-export const serverUrl=window.location.origin.includes('asira') ? 
+export const serverUrl=window.location.origin.includes('staging') ? 
 "https://staging.ayannah.co.id/api-lender/" :
 "https://cors-anywhere.herokuapp.com/http://staging.ayannah.co.id/api-lender/";
 
-export const serverUrlBorrower=window.location.origin.includes('asira') ? 
+export const serverUrlBorrower=window.location.origin.includes('staging') ? 
 "https://staging.ayannah.co.id/api-borrower/" :
 "https://cors-anywhere.herokuapp.com/https://staging.ayannah.co.id/api-borrower/";
 
