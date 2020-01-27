2c46dd973c650f1f43522ee71719b25c7629e8ec
Albert J <55692015+albertjulian-Ayannah@users.noreply.github.com>
2020-01-27 19:38:42 +0700

    Update url.js

--- src/components/url.js
+++ src/components/url.js
@@ -8,11 +8,11 @@
 //  export const serverUrlBorrower="https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/"
 
 export const serverUrl=window.location.origin.includes('asira') ? 
-"https://asira.ayannah.co.id/api-lender/" :
-"https://cors-anywhere.herokuapp.com/http://asira.ayannah.co.id/api-lender/";
+"http://asira.ayannah.com/api-lender/" :
+"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-lender/";
 
 export const serverUrlBorrower=window.location.origin.includes('asira') ? 
-"https://asira.ayannah.co.id/api-borrower/" :
-"https://cors-anywhere.herokuapp.com/https://asira.ayannah.co.id/api-borrower/";
+"http://asira.ayannah.com/api-borrower/" :
+"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/";
 
 
