42e82d2dd6cdbb9f82ee850cd89a230edb60431b
albertjulian-Ayannah <ajulian@ayannah.com>
2020-02-12 20:37:29 +0700

    urlFixed

--- src/components/url.js
+++ src/components/url.js
@@ -7,13 +7,13 @@
 //  export const serverUrl="https://virtserver.swaggerhub.com/ayannahindonesia/Asira_Lender/1.0.0/"
 //  export const serverUrlBorrower="https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/"
 
-export const serverUrl=window.location.origin.includes('staging') ? 
-"https://staging.ayannah.co.id/api-lender/" :
-"https://cors-anywhere.herokuapp.com/http://staging.ayannah.co.id/api-lender/";
+export const serverUrl=window.location.origin.includes('asira') ? 
+"http://asira.ayannah.com/api-lender/" :
+"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-lender/";
 
-export const serverUrlBorrower=window.location.origin.includes('staging') ? 
-"https://staging.ayannah.co.id/api-borrower/" :
-"https://cors-anywhere.herokuapp.com/https://staging.ayannah.co.id/api-borrower/";
+export const serverUrlBorrower=window.location.origin.includes('asira') ? 
+"http://asira.ayannah.com/api-borrower/" :
+"https://cors-anywhere.herokuapp.com/http://asira.ayannah.com/api-borrower/";
 
 
 
